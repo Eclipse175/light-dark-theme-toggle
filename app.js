57552413deda19a49dd6f3cb43ffd2ffbe23ddbd
Eclipse175 <fahimkhobar2004@gmail.com>
2024-08-03 18:51:13 +0300

    added a text to speech feature to this page

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const toggleTheme = document.getElementById('toggle-theme');
 const headings = document.querySelector('h1')
 const text = document.querySelector('p');
-const bodyColor = document.querySelector('body');   
-
-let isDarkMode = false;
+const bodyColor = document.querySelector('body');
+const convertButton = document.getElementById('convert-button');
+const inputField = document.getElementById('inputField');
 
 toggleTheme.addEventListener('click', function() {
     bodyColor.classList.toggle('dark-mode');
 });
+
+convertButton.addEventListener('click', function() {
+    playSpeech(inputField.value);
+});
+
+function playSpeech(text) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'hi-IN';
+    utterance.addEventListener('end', function() {
+        inputField.disabled = false;
+    });
+    inputField.disabled = true;
+    speechSynthesis.speak(utterance);
+}   
+
+//to prevent it from speaking when the page reloads 
+window.onbeforeunload = function() {
+    window.speechSynthesis.cancel();
+};
